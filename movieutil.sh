0f6ff7266f5544348aa8105cf8f857e09f33d5d5
kepler <githubkepler.50s@gishpuppy.com>
2015-12-04 18:59:31 +0530

    added director and GLOB

--- movieutil.sh
+++ movieutil.sh
@@ -14,7 +14,7 @@
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
 #       CREATED: 12/01/2015 12:50
-#      REVISION:  2015-12-03 14:30
+#      REVISION:  2015-12-04 15:55
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
@@ -118,8 +118,7 @@ if [[ -n "$OPT_TEST" ]]; then
 fi
 
 
-# this one matches from the start but assumes the title is exact, only year is not known.
-# NOTE: Year should NOT be mentioned,
+# this one matches from the start but assumes the last name is exact. Should be init upper case (proper case).
 # returns zero, or >=1  rows.
 last_name_equals ()
 {
@@ -131,9 +130,9 @@ last_name_equals ()
 $SQLITE $MYDATABASE <<!
 .mode tabs
 .headers off
-      select $MYCOL from $MYTABLE where $MYCOL like "${PATT}, %";
+      select $MYCOL from $MYTABLE where $MYCOL GLOB "${PATT}, *";
 !
-}	# ----------  end of function title_starting  ----------
+}	# ----------  end of function last_name_equals  ----------
 
 # this fully matches the name, and a (IVX) must be supplied if the db has it
 name_equals ()
@@ -148,7 +147,7 @@ $SQLITE $MYDATABASE <<!
 .headers off
       select $MYCOL from $MYTABLE where $MYCOL = "${PATT}";
 !
-}	# ----------  end of function title_starting  ----------
+}	# ----------  end of function name_equals  ----------
 
 # this is for those cases where the name in the DB contains a "(IVX)"
 #
@@ -162,9 +161,9 @@ name_starting ()
 $SQLITE $MYDATABASE <<!
 .mode tabs
 .headers off
-      select $MYCOL from $MYTABLE where $MYCOL like "${PATT}%";
+      select $MYCOL from $MYTABLE where $MYCOL GLOB "${PATT}*";
 !
-}	# ----------  end of function title_starting  ----------
+}	# ----------  end of function name_starting  ----------
 # this fully matches the altname, but no IVX is to be supplied, so multiples can be returned
 altname_equals ()
 {
@@ -178,16 +177,16 @@ $SQLITE $MYDATABASE <<!
 .headers off
       select name from $MYTABLE where $MYCOL = "${PATT}";
 !
-}	# ----------  end of function title_starting  ----------
+}	# ----------  end of function altname_equals  ----------
 
-# prints cast of movie and charactre given exact movie name.
+# prints cast of movie and character given exact actor name
 # may pass in  column names
 # may specify that you want headers (for csvlook)
-movie_cast_exact ()
-{
+# may actuall want ordered by year, which we don't have as a separate column here.
+movie_cast_exact () {
 
     opt_headers=off
-    opt_columns="name,character"
+    opt_columns="name, character"
     while [[ $1 = -* ]]; do
         case "$1" in
             -c|--columns)   shift
@@ -219,4 +218,151 @@ $SQLITE $MYDATABASE <<!
 .headers $opt_headers
       select $opt_columns from $MYTABLE where $MYCOL = "${PATT}";
 !
-}	# ----------  end of function title_starting  ----------
+}	# ----------  end of function movie_cast_exact  ----------
+
+# prints cast of movie and character given exact actor name
+# may pass in  column names
+# may specify that you want headers (for csvlook)
+# may actuall want ordered by year, which we don't have as a separate column here.
+movies_of_actor () {
+
+    opt_headers=off
+    opt_columns="title, character"
+    while [[ $1 = -* ]]; do
+        case "$1" in
+            -c|--columns)   shift
+                opt_columns=$1
+                #echo "columns is $opt_columns" 1>&2
+                shift
+                ;;
+            -H|--headers)   shift
+                opt_headers=$1
+                #echo "headers is $opt_headers" 1>&2
+                shift
+                ;;
+            *)
+                echo "Error: Unknown option: $1" >&2   # rem _
+                echo "Use -h or --help for usage"
+                exit 1
+                ;;
+        esac
+    done
+
+    PATT="$*"
+    [[ -z "$PATT" ]] && { echo "Error: movie blank." 1>&2; return 1; }
+
+    MYTABLE="cast"
+    MYCOL="name"
+      echo "Q is: select $opt_columns from $MYTABLE where $MYCOL = ${PATT};" 1>&2
+$SQLITE $MYDATABASE <<!
+.mode tabs
+.headers $opt_headers
+      select $opt_columns from $MYTABLE where $MYCOL = "${PATT}";
+!
+}	# ----------  end of function movies_of_actor  ----------
+
+#-------------------------------------------------------------------------------
+# DIRECTOR queries
+#-------------------------------------------------------------------------------
+# this one matches from the start but assumes the last name is exact. Should be init upper case (proper case).
+# returns zero, or >=1  rows.
+director_last_name_equals ()
+{
+    PATT="$*"
+    [[ -z "$PATT" ]] && { echo "Error: PATT blank." 1>&2; return 1; }
+
+    MYTABLE="director"
+    MYCOL="name"
+$SQLITE $MYDATABASE <<!
+.mode tabs
+.headers off
+      select $MYCOL from $MYTABLE where $MYCOL GLOB "${PATT}, *";
+!
+}	# ----------  end of function director_last_name_equals  ----------
+
+# this fully matches the name, and a (IVX) must be supplied if the db has it
+director_name_equals ()
+{
+    PATT="$*"
+    [[ -z "$PATT" ]] && { echo "Error: PATT blank." 1>&2; return 1; }
+
+    MYTABLE="director"
+    MYCOL="name"
+$SQLITE $MYDATABASE <<!
+.mode tabs
+.headers off
+      select $MYCOL from $MYTABLE where $MYCOL = "${PATT}";
+!
+}	# ----------  end of function director_name_equals  ----------
+
+# this is for those cases where the name in the DB contains a "(IVX)"
+#
+director_name_starting ()
+{
+    PATT="$*"
+    [[ -z "$PATT" ]] && { echo "Error: PATT blank." 1>&2; return 1; }
+
+    MYTABLE="director"
+    MYCOL="name"
+$SQLITE $MYDATABASE <<!
+.mode tabs
+.headers off
+      select $MYCOL from $MYTABLE where $MYCOL GLOB "${PATT}*";
+!
+}	# ----------  end of function director_name_starting  ----------
+
+# this fully matches the altname, but no IVX is to be supplied, so multiples can be returned
+director_altname_equals ()
+{
+    PATT="$*"
+    [[ -z "$PATT" ]] && { echo "Error: PATT blank." 1>&2; return 1; }
+
+    MYTABLE="director"
+    MYCOL="newname"
+$SQLITE $MYDATABASE <<!
+.mode tabs
+.headers off
+      select name from $MYTABLE where $MYCOL = "${PATT}";
+!
+}	# ----------  end of function director_altname_equals  ----------
+
+# prints movies of a director given exact name
+# may pass in  column names
+# may specify that you want headers (for csvlook)
+movies_of_director () 
+{
+
+    opt_headers=off
+    opt_columns="title"
+    while [[ $1 = -* ]]; do
+        case "$1" in
+            -c|--columns)   shift
+                opt_columns=$1
+                #echo "columns is $opt_columns" 1>&2
+                shift
+                ;;
+            -H|--headers)   shift
+                opt_headers=$1
+                #echo "headers is $opt_headers" 1>&2
+                shift
+                ;;
+            *)
+                echo "Error: Unknown option: $1" >&2   # rem _
+                echo "Use -h or --help for usage"
+                exit 1
+                ;;
+        esac
+    done
+
+    PATT="$1"
+    [[ -z "$PATT" ]] && { echo "Error: movie blank." 1>&2; return 1; }
+
+    MYTABLE="movie_director"
+    MYCOL="name"
+      #echo "Q is: select $opt_columns from $MYTABLE where $MYCOL = ${PATT};" 1>&2
+$SQLITE $MYDATABASE <<!
+.mode tabs
+.headers $opt_headers
+      select $opt_columns from $MYTABLE where $MYCOL = "${PATT}";
+!
+}	# ----------  end of function movies_of_director  ----------
