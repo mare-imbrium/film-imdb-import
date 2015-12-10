8878af9437b61073356211f78b490367d3cda587
kepler <githubkepler.50s@gishpuppy.com>
2015-12-10 12:59:30 +0530

    added index

--- create_director.sh
+++ create_director.sh
@@ -7,16 +7,28 @@ INFILE=/Users/rahul/Downloads/imdb_data/directors.tsv
 
 echo creating table $MYTABLE
 $COMM $MYDATABASE << !
+DROP TABLE $MYTABLE ;
+
 CREATE TABLE $MYTABLE (
 	name VARCHAR, 
 	newname VARCHAR
 );
 !
 ./import_table.sh $MYTABLE $INFILE || exit
-echo creating unique index
+echo "$MYTABLE Imported"
+echo creating index - cannot create unique index since duplicates exist with different cases
 
-echo "CREATE UNIQUE INDEX ${MYTABLE}_name on $MYTABLE(name);"
+echo "CREATE INDEX ${MYTABLE}_name on $MYTABLE(name COLLATE NOCASE);"
+echo "CREATE INDEX ${MYTABLE}_newname on $MYTABLE(newname COLLATE NOCASE);"
 $COMM $MYDATABASE << !
-CREATE UNIQUE INDEX ${MYTABLE}_name on $MYTABLE(name);
+CREATE INDEX ${MYTABLE}_name on $MYTABLE(name COLLATE NOCASE);
+CREATE INDEX ${MYTABLE}_newname on $MYTABLE(newname COLLATE NOCASE);
 !
 echo Done
+echo "Running one or two checks ..."
+$COMM $MYDATABASE << !
+ select count(1) from $MYTABLE;
+     select * from $MYTABLE where name = "Hitchcock, Alfred (I)";
+     select * from $MYTABLE where newname = "Alfred Hitchcock";
+     select * from $MYTABLE where name LIKE  "Deford, Randy";
+!
